694dfae482b04dc836e05be189b2d412f4130648
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-09 14:06:23 -0800

    Experimenting with Lists arrays and creating a fibonnaci program

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Formats.Asn1;
+using System.Runtime.Versioning;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
+
+class Program {
+
+    static void Main(string [] arg) {
+           
+           var words = new List<string> {" hello ", "worlds ", "      cnsd okc"};
+           var documents = new List<int> {1,2, 3};
+            words.Sort();
+           foreach (var word in words)
+           {
+                Console.WriteLine(word.ToUpper());
+           }
+
+           for (int i = 0; i < words.Count; i++) //because it is a list of words, we can use .Length we use cound
+           {
+            Console.WriteLine(words[i].Trim());
+           }
+
+            words.Add("Shukra");
+            words.Add("Najib");
+            documents.Add(45);
+            
+          var array1 =  words.ToArray();
+          var array2 =  documents.ToArray();
+          
+          
+         for (int i = 0; i < array1.Length; i++) //if we want to use .length we have to convert to an array
+         {
+            Console.WriteLine(array1[i].TrimStart());
+           
+         }
+        
+
+        for (int i = 0; i < array2.Length; i++)
+        {
+            Console.WriteLine(array2[i]);
+        }
+         if (array1.Contains("worlds ")==true) { //trying to search our array also notice how i had to add the space to match my string
+            Console.WriteLine("yeerrpp");
+                
+        }
+
+        // adding to an array would require you to create a function that increases array size and also  copies the elements
+        //however if you convert the array into a list, you say on time complexity and do all that in one line
+         var jonathan = array1.ToList();
+
+         foreach (var item in jonathan)
+         {
+            Console.WriteLine(item.ToUpper());
+         }
+
+
+         //lets do a fibonnaci game to teach what it means
+
+         int count = 0;
+         var fibonacccii = new List<int> {1,1}; // create a list of the first two in the sequence
+        
+        while (count < 20) { 
+
+         var numberbefore = fibonacccii[fibonacccii.Count-1]; // grab the last one 
+         var numberclosest = fibonacccii[fibonacccii.Count-2];// grab the second last one 
+         fibonacccii.Add(numberbefore + numberclosest);
+         
+         Console.WriteLine(fibonacccii[count]); // prinr out the each
+         count++; // count up till 20 numbers in
+         
+         }
+         
+        // lets convert this to an array for practice
+        var array3 = fibonacccii.ToArray();
+
+        for (int i = 0; i < array3.Length; i++)
+        {
+            Console.WriteLine(array3[i]);
+        }
+
+        }
+
+        
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Lists in C#
  Lists and property experimenting
+
+ # C# Program README
+
+## Overview
+
+This C# program demonstrates various operations using lists, arrays, and a Fibonacci sequence example. It includes functionalities such as sorting, trimming, converting between lists and arrays, and generating Fibonacci numbers.
+
+## Getting Started
+
+Ensure you have a C# development environment set up to run the program.
+
+### Prerequisites
+
+- [.NET SDK](https://dotnet.microsoft.com/download) installed
+
+### Running the Program
+
+1. Clone the repository or download the program file.
+2. Open a terminal or command prompt.
+3. Navigate to the directory containing the program file.
+4. Compile and run the program using the following commands:
+
+   ```bash
+   dotnet build
+   dotnet run
+
+
+# Program Details
+Sorting and Trimming
+The program initializes a list of words and documents, sorts the words, and demonstrates both uppercasing and trimming of strings.
+
+# Array Operations
+The program converts lists to arrays, iterates through the elements, and checks for the presence of a specific element.
+
+# Fibonacci Sequence
+The program generates the first 20 numbers in the Fibonacci sequence, demonstrating the usage of lists to dynamically store sequence elements.
+
+# Additional Information
+The program utilizes basic C# constructs such as loops, conditional statements, and list/array operations.
+Feel free to explore the code and modify it for educational purposes or further experimentation.
